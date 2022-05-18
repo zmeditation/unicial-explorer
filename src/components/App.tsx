832740af6ab380f25875c2e01674e6d816ef9ef1
zmeditation <senior.zsoftware@gmail.com>
2022-05-18 07:16:04 +0100

    modified dcl related to unicial

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -15,61 +15,61 @@ import { isElectron } from '../integration/desktop'
 import { BigFooter } from './common/Layout/BigFooter'
 import BannerContainer from './banners/BannerContainer'
 import { LoadingRender } from './common/Loading/LoadingRender'
-import { Navbar } from './common/Layout/Navbar'
+// import { Navbar } from './common/Layout/Navbar'
 import { FeatureFlags, getFeatureVariant } from '../state/selectors'
 import StreamContainer from './common/StreamContainer'
 import { isMobile } from '../integration/browser'
 
 function mapStateToProps(state: StoreType): AppProps {
-  return {
-    hasStream: !!getFeatureVariant(state, FeatureFlags.Stream),
-    hasBanner: !!state.banner.banner,
-    sessionReady: !!state.session?.ready,
-    rendererReady: !!state.renderer?.ready,
-    error: !!state.error.error,
-    sound: true // TODO: sound must be true after the first click
-  }
+    return {
+        hasStream: !!getFeatureVariant(state, FeatureFlags.Stream),
+        hasBanner: !!state.banner.banner,
+        sessionReady: !!state.session?.ready,
+        rendererReady: !!state.renderer?.ready,
+        error: !!state.error.error,
+        sound: true // TODO: sound must be true after the first click
+    }
 }
 
 export interface AppProps {
-  hasStream: boolean
-  hasBanner: boolean
-  sessionReady: boolean
-  rendererReady: boolean
-  error: boolean
-  sound: boolean
+    hasStream: boolean
+    hasBanner: boolean
+    sessionReady: boolean
+    rendererReady: boolean
+    error: boolean
+    sound: boolean
 }
 
 const App: React.FC<AppProps> = (props) => {
-  const mobile = useMemo(() => isMobile(), [])
-  const small = useMobileMediaQuery()
+    const mobile = useMemo(() => isMobile(), [])
+    const small = useMobileMediaQuery()
 
-  if (props.hasStream && (small || mobile)) {
-    return <StreamContainer />
-  }
+    if (props.hasStream && (small || mobile)) {
+        return <StreamContainer />
+    }
 
-  if (props.error) {
-    return <ErrorContainer />
-  }
+    if (props.error) {
+        return <ErrorContainer />
+    }
 
-  if (props.rendererReady) {
-    return <React.Fragment />
-  }
+    if (props.rendererReady) {
+        return <React.Fragment />
+    }
 
-  if (props.sessionReady) {
-    return <LoadingRender />
-  }
+    if (props.sessionReady) {
+        return <LoadingRender />
+    }
 
-  return (
-    <div className={`WebsiteApp ${props.hasBanner ? 'withBanner' : ''}`}>
-      {!isElectron() && props.sound && <Audio track={`${process.env.PUBLIC_URL}/tone4.mp3`} play />}
-      <BannerContainer />
-      {!isElectron() && <Navbar />}
-      <LoginContainer />
-      {/* {!isElectron() && <BeginnersGuide />} */}
-      {!isElectron() && <BigFooter />}
-    </div>
-  )
+    return (
+        <div className={`WebsiteApp ${props.hasBanner ? 'withBanner' : ''}`}>
+            {!isElectron() && props.sound && <Audio track={`${process.env.PUBLIC_URL}/tone4.mp3`} play />}
+            <BannerContainer />
+            {/* {!isElectron() && <Navbar />} */}
+            <LoginContainer />
+            {/* {!isElectron() && <BeginnersGuide />} */}
+            {!isElectron() && <BigFooter />}
+        </div>
+    )
 }
 
 export default connect(mapStateToProps)(App)
--- src/components/common/Loading/LoadingRender.tsx
+++ src/components/common/Loading/LoadingRender.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 import './LoadingRender.css'
 
 export const LoadingRender = React.memo(function () {
-  return <div className="LoadingRender">
-    <img alt="loading" src={process.env.PUBLIC_URL + '/images/loading.gif'} width="150" height="150" />
-    <p>We are downloading the latest version of Decentraland. You'll be up and running in a few seconds!</p>
-  </div>
+    return (
+        <div className="LoadingRender">
+            <img alt="loading" src={process.env.PUBLIC_URL + '/images/unicial-logo.png'} width="150" height="150" />
+            <p>We are downloading the latest version of Unicial. You'll be up and running in a few seconds!</p>
+        </div>
+    )
 })
