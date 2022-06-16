d4b2553bb76eac898689e2e19cb0c9728dd2bc7e
zmeditation <senior.zsoftware@gmail.com>
2022-06-16 08:07:46 +0100

    change loading image as gif

--- src/components/common/Loading/LoadingRender.tsx
+++ src/components/common/Loading/LoadingRender.tsx
@@ -4,7 +4,7 @@ import './LoadingRender.css'
 export const LoadingRender = React.memo(function () {
     return (
         <div className="LoadingRender">
-            <img alt="loading" src={process.env.PUBLIC_URL + '/images/unicial-logo.png'} width="150" height="150" />
+            <img alt="loading" src={process.env.PUBLIC_URL + '/images/loader.gif'} width="150" height="150" />
             <p>We are downloading the latest version of Unicial. You'll be up and running in a few seconds!</p>
         </div>
     )
