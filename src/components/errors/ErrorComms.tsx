704865dd5ffcedd4d77d57f5d445fa7afd12f643
zmeditation <senior.zsoftware@gmail.com>
2022-06-16 16:10:43 +0100

    Modify bg, image and button style in error page

--- src/components/errors/ErrorComms.tsx
+++ src/components/errors/ErrorComms.tsx
@@ -1,41 +1,42 @@
 import React from 'react'
 import './errors.css'
-import errorImage from '../../images/errors/error-robotdown.png'
+import errorImage from '../../images/errors/error-world.png'
 
 export const ErrorComms: React.FC = () => (
-  <div id="error-comms" className="error-container">
-    <div className="error-background" />
-    <div className="errormessage">
-      <div className="errortext col">
-        <div className="error">Oops</div>
-        <div className="communicationslink">
-          A communication link could not be
-          <br />
-          established with other peers
+    <div id="error-comms" className="error-container">
+        <div className="error-background" />
+        <div className="errormessage">
+            <div className="errortext col">
+                <div className="error">Oops</div>
+                <div className="communicationslink">
+                    A communication link could not be
+                    <br />
+                    established with other peers
+                </div>
+                <div className="givesomedetailof">
+                    This might be because you are behind a restrictive network firewall, or a temporary problem with the
+                    selected realm. <br />
+                    <br />
+                    If you have any ad blocking extensions try turning them off for this site, and then reload.
+                    <br />
+                    You can also try a different realm.
+                </div>
+                <div className="cta">
+                    <button
+                        className="retry"
+                        onClick={() => {
+                            window.location.reload()
+                        }}
+                    >
+                        Reload
+                    </button>
+                </div>
+            </div>
+            <div className="errorimage col">
+                <div className="imagewrapper">
+                    <img alt="" className="error-image" src={errorImage} />
+                </div>
+            </div>
         </div>
-        <div className="givesomedetailof">
-          This might be because you are behind a restrictive network firewall, or a temporary problem with the selected realm. <br />
-          <br />
-          If you have any ad blocking extensions try turning them off for this site, and then reload.
-          <br />
-          You can also try a different realm.
-        </div>
-        <div className="cta">
-          <button
-            className="retry"
-            onClick={() => {
-              window.location.reload()
-            }}
-          >
-            Reload
-          </button>
-        </div>
-      </div>
-      <div className="errorimage col">
-        <div className="imagewrapper">
-          <img alt="" className="error-image" src={errorImage} />
-        </div>
-      </div>
     </div>
-  </div>
 )
